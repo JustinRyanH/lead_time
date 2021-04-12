773ff791ab386c60beb9ce6bebbe1516e0f9fffa
Justin Hurst <jhurst@mavenlink.com>
2021-04-12 16:29:08 -0700

    refactor: Include simple date

--- lib/run.rb
+++ lib/run.rb
@@ -33,18 +33,19 @@ require_relative './commit_collector'
 collector = CommitCollector.new()
 deployments = NewRelic::load_deployments(application_id: APP_ID, commit_collector: collector, api_key: NEW_RELIC_API_KEY)
 
-headers = ["Deployment Sha", "Deployment Time", "Commit Sha", "Commit Time", "Author", "Time Till Deploy (Minutes)"]
+headers = ["Deployment Date", "Deployment Sha", "Deployment Time", "Commit Sha", "Commit Time", "Author", "Time Till Deploy (Minutes)"]
 CSV.open("./deployments.csv", 'w') do |csv|
   csv << headers
   total_deployments = deployments.raw.count
   current_deployment = 0
-  deployments.raw[0..100].each do |deployment|
+  deployments.raw[0..10].each do |deployment|
     puts "#{total_deployments - current_deployment} Deployments to go"
     deployment_time = deployment.time
+    deployment_date = deployment.date
     deployment_sha = deployment.sha
     deployments.get_commits(deployment.id)&.each do |commit|
       ttd = ((deployment_time - commit.time) * 24 * 60).to_i
-      csv << [deployment_sha, deployment_time, commit.sha, commit.time, commit.author.email, ttd]
+      csv << [deployment_date, deployment_sha, deployment_time, commit.sha, commit.time, commit.author.email, ttd]
     end
     current_deployment += 1
   end
